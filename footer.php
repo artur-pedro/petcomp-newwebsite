133c471c3adc151d780a15a50d48301b03a19478
oficialjoseguilherme <oficialjoseguilherme@gmail.com>
2025-01-13 12:08:38 -0300

    DEV Footer: Parte do meio e parte de baixo do footer

--- footer.php
+++ footer.php
@@ -5,25 +5,104 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/footer.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
 </head>
 
 <body>
     <footer>
 
-        <div class="footer-header">
-            <div class="logos">
+        <div class="footer-top">
+            <div class="footer-logos">
                 <img src="img/logo-pet-branco.png" alt="logo-pet-branco">
                 <img src="img/logo-ufma-branco.png" alt="logo-ufma-branco">
             </div>
             <hr>
         </div>
 
-        <div>
-
+        <div class="footer-mid">
+            <div class="footer-elms">
+                <h2>
+                    PETComp
+                </h2>
+                <ul>
+                    <li>
+                        <a href="">Sobre nós</a>
+                    </li>
+                    <li>
+                        <a href="">Integrantes</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-elms">
+                <h2>
+                    Projetos
+                </h2>
+                <ul>
+                    <li>
+                        <a href="">Monitoria</a>
+                    </li>
+                    <li>
+                        <a href="">Fáb. Software</a>
+                    </li>
+                    <li>
+                        <a href="">Minicursos e Oficinas</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-elms">
+                <h2>
+                    Publicações
+                </h2>
+                <ul>
+                    <li>
+                        <a href="">Trabalhos Publicados</a>
+                    </li>
+                    <li>
+                        <a href="">Revista PETComp</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-elms">
+                <h2>
+                    Produtos
+                </h2>
+                <ul>
+                    <li>
+                        <a href="">Biblioteca PETComp</a>
+                    </li>
+                    <li>
+                        <a href="">PODCast</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-elms">
+                <h2>
+                    Links
+                </h2>
+                <ul>
+                    <li>
+                        <a href="">Departamento de computação</a>
+                    </li>
+                    <li>
+                        <a href="">UFMA</a>
+                    </li>
+                </ul>
+            </div>
         </div>
         
-        <div>
-
+        <div class="footer-bottom">
+            <hr>
+            <div class="footer-logos">
+                <a href="https://www.instagram.com/petcompufma/">
+                    <img src="img/logo-instagram.png" alt="logo-instagram">
+                </a>
+                <a href="#">
+                    <img src="img/logo-facebook.png" alt="logo-facebook">
+                </a>
+                <a href="#">
+                    <img src="img/logo-twitter.png" alt="logo-twitter">
+                </a>
+            </div>
         </div>
 
     </footer>
