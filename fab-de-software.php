13af61e00e7af1922f0688e0b3a0edc38fcfc868
Arthur <guy.art123.ad@gmail.com>
2025-01-13 12:11:48 -0300

    inicio pagina fab.software

---
+++ fab-de-software.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title> PETComp </title>
+
+  <link rel="icon" href="./assets/images/logos/PETComp.png">
+
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Mada:wght@400;700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="css/fab-de-software.css">
+
+</head>
+
+<body>
+  <?php include('header.php') ?>
+  <main>
+    <div>
+      <div class="title-section">
+        <h1>Fábrica de Software</h1>
+        <h2>Engenharia de Software no PETComp</h2>
+
+        <div class="acess-title">
+            <a href="#" class="acess-link"> Pagina Inicial -> </a>
+            <a href="#" class="acess-link"> Projetos -> </a>
+            <h4> Fáb.Software </h4>
+        </div>
+
+      </div>
+
+      <div class="text-fab-software">
+        <p>A Engenharia de Softwares é uma das vertentes mais fortes na área de Ciência da Computação. A construção de um software pode ser para fins administrativos, de pesquisa, entretenimento, etc. A atividade visa a coleta, criação e manutenção de softwares para a UFMA, também visa solucionar problemáticas da comunidade e projetos apoiados pela IES. Além disso, a atividade colabora juntamente para o ensino de tecnologias aos integrantes do grupo PET e para o compartilhamento desse conhecimento fazendo o uso de políticas de troca de conhecimento, pesquisa e desenvolvimento de tecnologias, e a extensão tecnológica.</p>
+        <p>A atividade da continuidade às políticas já realizadas pelo PETComp. Os projetos serão adotados por toda a equipe, de forma que cada um se responsabiliza por algumas sub funcionalidades, usando de processos de desenvolvimento de software hábil a ser pesquisado pelos alunos, bem como metodologias para a gestão de atividades. O software que apresentar melhor desempenho no processo de ensino e aprendizagem será escolhido e produzido. Poderão ser produzidos durante o desenvolvimento da atividade: objetos de aprendizado, aplicações móveis para fins diversos, sistemas de informação, e sistemas computacionais para atender demandas, através das pesquisas realizadas em outras políticas desta proposta.</p>
+      </div>
+
+      <!--
+      <div class="sites-cards">
+        <div class="software-card">
+          <img src="./assets/images/logos/observatorio.svg" alt="">
+          <div class=" text">
+            <h2><a target="_blank" href="http://observatoriodesaudemental.com.br/">Observatório Saúde Mental</a></h2>
+            <div class="wrapper-labels">
+              <div class="label php">PHP</div>
+              <div class="label html">HTML</div>
+              <div class="label css">CSS</div>
+              <div class="label javascript">JavaScript</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="software-card">
+          <img src="./assets/images/logos/cocom.svg" alt="">
+          <div class="text">
+            <h2><a target="_blank" href="https://petcompufma.org/cocom/">COCOM</a></h2>
+            <div class="wrapper-labels">
+              <div class="label php">PHP</div>
+              <div class="label html">HTML</div>
+              <div class="label css">CSS</div>
+              <div class="label javascript">JavaScript</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="software-card">
+          <img src="./assets/images/logos/eacomp.svg" alt="">
+          <div class="text">
+            <h2><a target="_blank" href="https://petcompufma.org/eacomp/">EAComp</a></h2>
+            <div class="wrapper-labels">
+              <div class="label html">HTML</div>
+              <div class="label css">CSS</div>
+              <div class="label javascript">JavaScript</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="software-card">
+          <img src="./assets/images/logos/mamaprev.png" alt="">
+          <div class="text">
+            <h2>MAMAPrev</h2>
+            <div class="wrapper-labels">
+              <div class="label react">React-Native</div>
+              <div class="label javascript">JavaScript</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="software-card">
+          <img src="./assets/images/logos/aconchego.svg" alt="">
+          <div class="text">
+            <h2>Aconchego</h2>
+            <div class="wrapper-labels">
+              <div class="label react">React-Native</div>
+              <div class="label javascript">JavaScript</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="software-card">
+          <img src="./assets/images/logos/bauxita.svg" alt="">
+          <div class="text">
+            <h2><a target="_blank" href="http://bauxiteresidue.ufma.br/">Resíduo Bauxita</a></h2>
+            <div class="wrapper-labels">
+              <div class="label php">PHP</div>
+              <div class="label html">HTML</div>
+              <div class="label css">CSS</div>
+              <div class="label javascript">JavaScript</div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    -->
+    </div>
+  </main>
+
+  <?php include('footer.php') ?>
+  <script src="./js/js.js"></script>
+</body>
+
+</html>
