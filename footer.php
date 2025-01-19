ca88419669a771f6b69ee37157c3b3c29d242de3
rickeick <rickeick2016@gmail.com>
2025-01-19 12:49:34 -0300

    FIX Footer: Corrigindo espaçamento entre as listas

--- footer.php
+++ footer.php
@@ -1,24 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/footer.css">
-</head>
-
-<body>
-    <footer class="footer">
-        <div class="footer-linha">
-            dandadan
+<footer class="footer">
+    <div class="footer-bar">
+        <div class="footer-logos">
+            <img src="img/logo-pet-branco.png" alt="logo-pet-branco">
+            <img src="img/logo-ufma-branco.png" alt="logo-ufma-branco">
         </div>
-        <div>
-
+        <hr>
+    </div>
+    <div class="footer-mid">
+        <ul>
+            <li><h2>PETComp</h2></li>
+            <li><a href="">Sobre Nós</a></li>
+            <li><a href="">Integrantes</a></li>
+        </ul>
+        <ul>
+            <li><h2>Projetos</h2></li>
+            <li><a href="">Monitoria</a></li>
+            <li><a href="">Fáb. de Software</a></li>
+            <li><a href="">Minicursos e Oficinas</a></li>
+        </ul>
+        <ul>
+            <li><h2>Publicações</h2></li>
+            <li><a href="">Trabalhos Publicados</a></li>
+            <li><a href="">Revista PETComp</a></li>
+        </ul>
+        <ul>
+            <li><h2>Produtos</h2></li>
+            <li><a href="">Biblioteca PETComp</a></li>
+            <li><a href="">PODCast</a></li>
+        </ul>
+        <ul>
+            <li><h2>Links</h2></li>
+            <li><a href="">Departamento de Informática</a></li>
+            <li><a href="">UFMA</a></li>
+        </ul>
+    </div>
+    <div class="footer-bar">
+        <hr>
+        <div class="footer-logos">
+            <a href="https://www.instagram.com/petcompufma/">
+                <img src="img/logo-instagram.png" alt="logo-instagram">
+            </a>
+            <a href="#">
+                <img src="img/logo-facebook.png" alt="logo-facebook">
+            </a>
+            <a href="#">
+                <img src="img/logo-twitter.png" alt="logo-twitter">
+            </a>
         </div>
-        <div class="footer-linha">
-            kfaçldfjk
-        </div>
-    </footer>
-</body>
-    
-</html>
+    </div>
+</footer>
