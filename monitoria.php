b5387795334c1df4e87dfb413a15f5e113e2b458
GuilhermeSantosrr <carlos.guilhermesantos1@gmail.com>
2025-01-22 09:52:57 -0300

    Pagina de monitoria

---
+++ monitoria.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Monitoria</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/monitoria.css">
+    <link href="https://fonts.googleapis.com/css2?family=Mada:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <?php include('header.php') ?>
+  <main>
+    <div>
+        <div class="main-title">
+            <h1>Monitoria</h1>
+            <h2>Como o PETComp apoia os alunos de computação</h2>
+
+            <div class="pages-acessed">
+                <a href="#" class="link-acessed"> Pagina Inicial -> </a>
+                <a href="#" class="link-acessed"> Projetos -> </a>
+                <h4> Monitoria </h4>
+            </div>
+        </div>
+
+        <div class="paragraph">
+            <p>
+                Visando combater problemas, como o alto índice de evasão dos cursos da área de tecnologia e o baixo rendimento dos alunos nas primeiras disciplinas que envolvem computação e cálculo, foi criado o GAD (Grupo de Acompanhamento de Discentes). Essa atividade serve como um grupo de estudos para os discentes que estão com dificuldade, além de ajudar na interação dos mesmos com outros alunos de períodos mais avançados do curso. Assim, ajudando e motivando os discentes a continuar no curso e a se interessar pelas disciplinas: Algoritmos 1, Cálculo 1, Estrutura de Dados 1, Linguagem de Programação 1 e Matemática Discreta e Lógica, sendo essas disciplinas de grande importância para o curso. Essa atividade, além de trazer o ensino e extensão, também envolve a pesquisa, trazendo a formação criativa de novas práticas de ensino, apresentação e defesa de resultados.
+            </p>
+
+            <p>
+                A atividade é realizada 3 vezes por semana, durante duas horas em uma sala do Google Meet criada previamente. Os petianos responsáveis pela atividade ficam à disposição para responder às perguntas surgidas durante a aula e/ou durante a resolução dos exercícios e passam exercícios sobre o atual assunto dado naquele momento da disciplina, que podem ser respondidos durante a monitoria ou em um momento posterior. Ao final do semestre, que é quando acaba a monitoria, é dado um formulário para os alunos darem suas opiniões sobre a monitoria, possíveis formas de melhorar a explicação e abordagem do assunto e quais foram as dificuldades enfrentadas durante a monitoria.
+            </p>
+
+            <p>
+                Os principais resultados esperados após a realização dessa atividade são melhoria no rendimento dos discentes que participaram das monitorias, o desenvolvimento de novas metodologias de ensino e uma maior união dos petianos com o resto dos discentes do curso. Os arquivos da monitoria podem ser vistos neste <a href="#">link</a>.
+            </p>
+        </div>
+
+        <div class="monitoring-image">
+            <div class="image-left">
+                <img src="img/image 13.png">
+                <img src="img/image 14.png">
+                <img src="img/image 15.jpg">
+            </div>
+
+            <div class="image-right">
+                <img src="img/image 11.jpg">
+                <img src="img/image 12.jpg">
+                <img src="img/image 10.jpg">
+                <img src="img/image 16.jpg">
+            </div>
+    </div>
