bf6d5f7673a000e26b1f1cfc07b606456731baf8
franciscorcb <franciscostay177@gmail.com>
2025-01-28 12:07:56 -0300

    Continuando pagina eventos

--- eventos.php
+++ eventos.php
@@ -18,15 +18,44 @@
 <body>
     <?php include('header.php') ?>
     <main>
-        <div class="acess-title">
+        <div class= "caixa-principal">
             <h1>Eventos</h1>
-            <h2>Confira os principais eventos que o PETComp organiza</h2>
-            <a href="#" class="acess-link"> Pagina Inicial -> </a>
-            <h4> Eventos </h4>
+            <h2>Confira os eventos que nós promovemos!</h2>
+            <div class = "caminho">
+                <a href="#" class="acess-link"> Pagina Inicial -> </a>
+                <h4> Eventos </h4>
+            </div>
         </div>
-        <div>
-            <h4>Os eventos serão planejados através de reuniões entre o grupo. Na primeira reunião, um petiano é eleito líder do monitoramento das atividades a serem realizadas para que ocorra o evento. Este líder, em conjunto com o tutor, indica membros para auxiliá-lo diretamente, podendo inclusive, ser todos os membros do PET. O tutor atua como facilitador entre as várias entidades e atores da universidade.</h4>
+
+        <div class = "textos">
+            <h4 id = "t">Os eventos serão planejados através de reuniões entre o grupo. Na primeira reunião, um petiano é 
+                eleito líder do monitoramento das atividades a serem realizadas para que ocorra o evento. Este líder
+                , em conjunto com o tutor, indica membros para auxiliá-lo diretamente, podendo inclusive, ser todos 
+                os membros do PET. O tutor atua como facilitador entre as várias entidades e atores da universidade.
+            </h4>
+            <h4>
+            O grupo então define a programação baseada em minicursos, tutorias, palestras, competições, apresentação, 
+            rodas de discussão e laboratórios e define também comissões para cuidar de grandes tarefas do evento. A 
+            programação é baseada na tendência atual da área e tende a atender os anseios do público diretamente envolvido 
+            do curso de Ciência da Computação.
+            </h4>
+            <h4>Os eventos são então divulgados através de redes sociais em todos os cursos da área do Maranhão, em 
+                instituições públicas e particulares, assim como em instituições que possuem cursos técnicos na área. 
+                Durante os eventos, as comissões formadas atuam para garantir que a realização do evento ocorra de forma 
+                completamente ordenada.
+            </h4>
         </div>
+
+        <div class= "box-models">
+            <h1>Acalourada</h1>
+            <h4>A Acalourada Virtual é uma atividade de recepção dos estudantes ingressantes no curso de Ciência da 
+                Computação de forma totalmente virtual. Essa atividade já vem sendo desenvolvida de forma presencial 
+                há mais de 3 anos em conjunto com a Coordenação de Computação, o Departamento de Informática e o PET 
+                de Computação, e procura guiar os calouros através da universidade e do curso, além de mostrar as 
+                oportunidades que a universidade oferece tanto de forma pessoal como profissional.
+            </h4>
+        </div>
+
     </main>
 </body>
 </html>
