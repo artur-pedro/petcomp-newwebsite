f2998901dfea0c85feb688028634026c3d4aac75
franciscorcb <franciscostay177@gmail.com>
2025-03-13 09:59:41 -0300

    Finaliando pag eventos

---
+++ eventos.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title> Eventos </title>
+
+  <link rel="icon" href="./assets/images/logos/PETComp.png">
+
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Mada:wght@400;700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="css/eventos.css">
+
+</head>
+
+<body>
+    <?php include('header.php') ?>
+    <main>
+        <div class= "main-box">
+            <h1>Eventos</h1>
+            <h2>Confira os eventos que nós promovemos!</h2>
+            <div class = "path-text">
+                <a href="#" class="acess-link"> Pagina Inicial -> </a>
+                <h4> Eventos </h4>
+            </div>
+        </div>
+
+        <div class = "textos">
+            <h4 id = "t">Os eventos serão planejados através de reuniões entre o grupo. Na primeira reunião, um petiano é 
+                eleito líder do monitoramento das atividades a serem realizadas para que ocorra o evento. Este líder
+                , em conjunto com o tutor, indica membros para auxiliá-lo diretamente, podendo inclusive, ser todos 
+                os membros do PET. O tutor atua como facilitador entre as várias entidades e atores da universidade.
+            </h4>
+            <h4>
+            O grupo então define a programação baseada em minicursos, tutorias, palestras, competições, apresentação, 
+            rodas de discussão e laboratórios e define também comissões para cuidar de grandes tarefas do evento. A 
+            programação é baseada na tendência atual da área e tende a atender os anseios do público diretamente envolvido 
+            do curso de Ciência da Computação.
+            </h4>
+            <h4>Os eventos são então divulgados através de redes sociais em todos os cursos da área do Maranhão, em 
+                instituições públicas e particulares, assim como em instituições que possuem cursos técnicos na área. 
+                Durante os eventos, as comissões formadas atuam para garantir que a realização do evento ocorra de forma 
+                completamente ordenada.
+            </h4>
+        </div>
+
+        <div class = "noticias">
+
+            <div class= "box-models">
+                <img src="assets\images\pag-eventos\box-acalourada.png" alt="Ícone Acalourada">
+                <div class = "texts-content">
+                    <h1>Acalourada</h1>
+                    <h4>
+                        A Acalourada Virtual é uma atividade de recepção dos estudantes ingressantes no curso de Ciência da
+                        Computação de forma totalmente virtual. Essa atividade já vem sendo desenvolvida de forma presencial
+                        há mais de 3 anos em conjunto com a Coordenação de Computação, o Departamento de Informática e o PET
+                        de Computação, e procura guiar os calouros através da universidade e do curso, além de mostrar as
+                        oportunidades que a universidade oferece tanto de forma pessoal como profissional.
+                    </h4>
+                    <button>Saiba mais</button>
+                </div>
+            </div>
+
+            <div class= "box-models">
+                <img src="assets\images\pag-eventos\box-eacomp.png" alt="Ícone EACOMP">
+                <div class = "texts-content">
+                    <h1>EAComp</h1>
+                    <h4>
+                        O Encontro Acadêmico de Computação (EAComp) da UFMA é uma forma de receber novos alunos e familiarizá-los 
+                        com o curso de Graduação de Ciência da Computação da UFMA apresentando as oportunidades dentro da graduação. 
+                        O EAComp é organizado pelo Programa de Educação Tutorial do curso de Ciência da Computação (PETComp).
+                    </h4>
+                    <button>Saiba mais</button>
+                </div>
+            </div>
+
+            <div class= "box-models">
+                <img src="assets\images\pag-eventos\box-eea.svg" alt="Ícone EEA">
+                <div class = "texts-content">
+                    <h1>EEA</h1>
+                    <h4>
+                        As competências digitais tornaram-se essenciais para o processo de ensino e aprendizagem, envolvendo a utilização 
+                        segura e crítica das TDIC (Tecnologias Digitais de Informação e Comunicação) e da Internet para pesquisar, encontrar, 
+                        avaliar, processar, armazenar, utilizar, produzir, apresentar e trocar informações, inclusive em rede. Nesse contexto, 
+                        o I Encontro sobre Experiências Acadêmicas da UFMA (I EEA da UFMA) é um evento com o objetivo de permitir que toda a 
+                        comunidade da Universidade Federal do Maranhão (UFMA) e demais Instituições de Ensino Superior públicas do Maranhão 
+                        possam trocar experiências, aprender e divulgar ações bem-sucedidas no contexto do desenvolvimento e disponibilização 
+                        de produções técnicas inovadoras.
+                    </h4>
+                    <button>Saiba mais</button>
+                </div>
+            </div>
+
+            <div class= "box-models">
+                <img src="assets\images\pag-eventos\box-enepet.png" alt="Ícone ENEPET">
+                <div class = "texts-content">
+                    <h1>ENEPET</h1>
+                    <h4>
+                        O Encontro Nordestino dos Grupos do Programa de Educação Tutorial (ENEPET) é um evento em que a comunidade petiana da 
+                        região Nordeste se encontra para discutir questões relacionadas ao Programa de Educação Tutorial (PET) na região. O 
+                        encontro é caracterizado por assembleias, grupos de discussão e trabalhos, apresentação de trabalhos acadêmicos, 
+                        atividades culturais, além da troca de experiências entre discentes e tutores nordestinos.
+                    </h4>
+                    <button>Saiba mais</button>
+                </div>
+            </div>
+
+            <div class= "box-models">
+                <img src="assets\images\pag-eventos\box-ercemapi.svg" alt="Ícone ERCEMAPI">
+                <div class = "texts-content">
+                    <h1>ERCEMAPI</h1>
+                    <h4>
+                        A primeira edição da Escola Regional de Computação dos Estados do Ceará, Maranhão e Piauí (ERCEMAPI) ocorreu em 2007 e 
+                        foi sediada em Fortaleza, CE. O evento busca promover a integração entre os três estados e tem como objetivo disseminar 
+                        o conhecimento técnico e científico sobre temas e assuntos de vanguarda na área de Computação. As Escolas Regionais são 
+                        eventos promovidos pela Sociedade Brasileira de Computação (SBC). A ERCEMAPI visa se consolidar como um evento de 
+                        referência nos Estados do Maranhão, Ceará e Piauí e região. Além disso, o evento colabora e contribui com a consolidação
+                        dos Programas de Pós-Graduação da região.
+                    </h4>
+                    <button>Saiba mais</button>
+                </div>
+            </div>
+
+            <div class= "box-models">
+                <img src="assets\images\pag-eventos\box-ejim.svg" alt="Ícone EJIM">
+                <div class = "texts-content">
+                    <h1>EJIM</h1>
+                    <h4>
+                        A Jornada Informática do Maranhão (JIM) é um evento de caráter acadêmico e técnico-científico com o objetivo de 
+                        propiciar um espaço de intercâmbio entre os diversos atores dos setores de Informática da região, apresentando e 
+                        discutindo temas que abordam a aplicação da Computação para a solução de problemas do Estado do Maranhão, bem como as 
+                        pesquisas em Informática realizadas nas instituições do estado, disponibilizando aos participantes a possibilidade de 
+                        acesso a temas e discussões recentes na área, através de diversas atividades, como palestras, minicursos, painéis de 
+                        discussão e apresentação de trabalhos científicos.
+                    </h4>
+                    <button>Saiba mais</button>
+                </div>
+            </div>
+
+            <div class= "box-models">
+                <img src="assets\images\pag-eventos\box-enapet.jpg" alt="Ícone ENAPET">
+                <div class = "texts-content">
+                    <h1>ENAPET</h1>
+                    <h4>
+                        Todos os anos, o Encontro Nacional do Programa de Educação Tutorial (ENAPET) reúne discentes, docentes e interlocutores 
+                        vinculados ao programa com o objetivo de discutir, coletivamente, temas e questões relevantes para a manutenção e o 
+                        desenvolvimento do PET nacionalmente.
+                    </h4>
+                    <button>Saiba mais</button>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
