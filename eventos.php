8b63f2499b3fe437d2792963dcab805c870f6e29
franciscorcb <franciscostay177@gmail.com>
2025-04-02 11:36:26 -0300

    atualizando eventos

--- eventos.php
+++ eventos.php
@@ -1,24 +1,10 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-<head>
-
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title> Eventos </title>
-
-  <link rel="icon" href="./assets/images/logos/PETComp.png">
-
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Mada:wght@400;700&display=swap" rel="stylesheet">
-
-  <link rel="stylesheet" href="css/globals.css">
-
-  <link rel="stylesheet" href="css/eventos.css">
-
-</head>
+<?php 
+    $title = "PETComp";
+    $cssFiles = ['css/eventos.css'];
+    include "head.php";
+?>
 
 <body>
     <?php include('header.php') ?>
