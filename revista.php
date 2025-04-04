b6b2673533fcca1a4be7c57bb4764a43ce01839c
GeovannaMLima <geovanna.imlima@gmail.com>
2025-04-04 12:03:08 -0300

    criandopagRevista

--- revista.php
+++ revista.php
@@ -1,8 +1,9 @@
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <?php 
-    $title = "Revista";
+    $title = "Sobre nós";
     $cssFiles = ['css/revista.css'];
     include 'head.php';
 ?>
@@ -14,16 +15,18 @@
         <h2>Revista</h2>
         <h3>Confira a publicação anual do PETComp</h3>
         <h4><a href="index.php">Página Inicial</a></h4>
-        <h4> -> Revista</h4> 
-        
-         
+        <h4> -> Publicaçoes</h4>
+        <h4> -> Revista</h4>
     </div>
 
     <div class="container-body">
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat iaculis neque. Nulla facilisi. Pellentesque eget ultrices lacus, vitae placerat massa. Quisque volutpat velit vulputate, viverra enim quis, vehicula leo. Nullam tincidunt sapien libero, eu aliquet arcu lacinia id. Quisque id volutpat sapien, non volutpat orci. Phasellus sollicitudin, sem eu gravida euismod, ante nunc cursus mi, cursus cursus massa tellus feugiat magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam finibus diam eget lorem pharetra, quis viverra velit tincidunt. Mauris euismod consectetur interdum. Duis vitae posuere tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus eget ipsum enim. Suspendisse vel eleifend lacus. Integer consequat lorem vel ex faucibus, sed viverra nisi placerat. Pellentesque quis nisl nec erat aliquam posuere. Curabitur et vestibulum nunc. Vestibulum vel tellus sagittis eros imperdiet eleifend non quis leo. Sed vitae leo in diam imperdiet fermentum. Donec aliquam porttitor dolor sit amet fringilla. Ut pretium vulputate felis a luctus. Aliquam sit amet massa in purus tincidunt bibendum eu nec velit. Donec in mauris at lectus posuere consectetur. Curabitur mollis pretium nisi non interdum. Ut venenatis quam et dolor congue, id tempus nisl cursus. Ut mollis nibh sed felis sodales, nec congue magna lacinia. Nullam id fringilla mauris, vel blandit augue.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat iaculis neque. Nulla facilisi. Pellentesque eget ultrices lacus, vitae placerat massa. Quisque volutpat velit vulputate, viverra enim quis, vehicula leo. Nullam tincidunt sapien libero, eu aliquet arcu lacinia id. Quisque id volutpat sapien, non volutpat orci. Phasellus sollicitudin, sem eu gravida euismod, ante nunc cursus mi, cursus cursus massa tellus feugiat magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam finibus diam eget lorem pharetra, quis viverra velit tincidunt. Mauris euismod consectetur interdum. Duis vitae posuere tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus eget ipsum enim. Suspendisse vel eleifend lacus. Integer consequat lorem vel ex faucibus, sed viverra nisi placerat. Pellentesque quis nisl nec erat aliquam posuere. Curabitur et vestibulum nunc.
             </p>
-            
+            <p>
+            ue quis nisl nec erat aliquam posuere. Curabitur et vestibulum nunc. Vestibulum vel tellus sagittis eros imperdiet eleifend non quis leo. Sed vitae leo in diam imperdiet fermentum. Donec aliquam porttitor dolor sit amet fringilla. Ut pretium vulputate felis a luctus. Aliquam sit amet massa in purus tincidunt bibendum eu nec velit. Donec in mauris at lectus posuere consectetur. Curabitur mollis pretium nisi non interdum. Ut venenatis quam et dolor congue, id tempus nisl cursus. Ut mollis nibh sed felis sodales, nec congue magna lacinia. Nullam id fringilla mauris, vel blandit augue.<a href="#">link</a>.
+            </p>
+           
     </div>
 
 
