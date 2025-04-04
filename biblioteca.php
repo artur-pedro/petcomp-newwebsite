44784a91932dd1af7ddf8d125bd93713ad34466d
Yan Felipe <yanfelipebp@gmail.com>
2025-04-04 10:31:15 -0300

    criação página biblioteca

---
+++ biblioteca.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<?php 
+    $title = "Biblioteca";
+    $cssFiles = [''];
+    include "head.php";
+?>
+
+<body>
+    <?php include('header.php') ?>
+    <main>
+    <div class="container-header">    
+        <h2>Biblioteca</h2>
+        <h3>Veja as monitorias que promovemos</h3>
+        <h4><a href="index.php">Página Inicial</a></h4>
+        <h4> -> Biblioteca PETComp</h4>
+
+    </div>
+        
+        <div class="container-body">
+            <p>Com o objetivo de apoiar tanto a comunidade acadêmica quanto a local, o PETComp oferece uma variedade de documentos e materiais de apoio que estão disponíveis para consulta a qualquer momento. Esses recursos foram cuidadosamente selecionados para atender às necessidades de estudantes, professores e pesquisadores, e podem ser acessados facilmente através do nosso site. Abaixo, você encontrará uma coleção de arquivos de auxílio educacional, que abrangem diferentes áreas do conhecimento e têm o intuito de complementar e enriquecer o aprendizado de todos.  
+            </p>        
+        </div>
+
+        <div class = "noticias">
+            <div class= "box-models" >
+
+            </div>
+            <div class="box-models">
+
+            </div>
+            <div class="box-models">
+
+            </div>
+            <div class="box-models">
+
+            </div>
+            <div class="box-models">
+
+            </div>
+        </div>
+    
+    </main>
+</body>
+<?php include('footer.php') ?>
+  <script src="./js/js.js"></script>
+</html>
