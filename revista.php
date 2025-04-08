f207eea95fca5011a749dd944b617e281d39226d
GeovannaMLima <geovanna.imlima@gmail.com>
2025-04-08 11:56:32 -0300

    revista

--- revista.php
+++ revista.php
@@ -49,6 +49,7 @@
 
 
 
+<<<<<<< Updated upstream
 
         <!-- Inputs de controle do slide -->
     <input type="radio" name="carousel" id="slide1" checked>
@@ -81,6 +82,8 @@
         <label for="slide1" class="arrow" id="right3">&#10095;</label>
         </div>
     </div>
+=======
+>>>>>>> Stashed changes
 
 
     <?php include 'footer.php'; ?>
